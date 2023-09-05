6c951ae3e105152eb8da982c0cdbe6b83ec2d883
Sofie Helene Karlsen <sofiehk@gmail.com>
2023-09-05 11:10:46 +0200

    sletter unødvendig kode

--- 2023-08-29/index.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById('input')
-const paragraph = document.querySelector('.paragraph')
-const button = document.getElementById('button')
-
-button.addEventListener('click', () => {
-  const inputValue = input.value
-  paragraph.innerHTML = inputValue
-})
